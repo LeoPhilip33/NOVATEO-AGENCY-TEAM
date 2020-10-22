723de599569bc6d317b08cfcf818e7dfc3ad17f5
PHILIP Léo <leo.philip33.bordeaux@gmail.com>
2020-10-22 19:47:46 +0200

    push qr code

--- js/main.js
+++ js/main.js
@@ -2,17 +2,16 @@
 const loader = document.getElementById('loader');
 
 
-const loaded = ()=>{
-	loader.classList.add('active');
+const loaded = () => {
+    loader.classList.add('active');
     $('.img-accueil').addClass('active');
     $('.first-nilk').addClass('active');
 }
 
-$('.burger-container').click(()=>{
-	$('.burger').toggleClass('active');
-	$('.menu-container').toggleClass('active');
+$('.burger-container').click(() => {
+    $('.burger').toggleClass('active');
+    $('.menu-container').toggleClass('active');
 })
-
 
 
 
